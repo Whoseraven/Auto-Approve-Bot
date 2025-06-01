974eb936497c679502c3b58a918a98cf6b26c012
Whoseraven <botraven75@gmail.com>
2025-06-01 11:03:53 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -17,7 +17,7 @@ async def web_server():
 class Bot(Client):
     def __init__(self):
         super().__init__(
-            "techifybots",
+            "Kirito_Bots",
             api_id=API_ID,
             api_hash=API_HASH,
             bot_token=BOT_TOKEN,
